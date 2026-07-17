8ff08c9fa8d7b9f09964a0c15bcf8bb0a123cc37
2Creative4Name <kvartal959880@gmail.com>
2026-07-17 18:04:14 +0200

    E: add fps

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
             pygame.Surface.fill(screen, (200,0,50))
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
+            
         
     
     
