35c6198d6f1b8eb9c4359355655f692ca10b80a6
2Creative4Name <kvartal959880@gmail.com>
2026-07-25 15:02:55 +0200

    G: start on asteroids

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import pygame
 from constants import *
 from logger import log_state
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 def main() -> None:
     
     pygame.init()
     
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
+    asteroid_field = AsteroidField()
     dt = 0.0
 
     
@@ -28,6 +36,8 @@ def main() -> None:
         updatable.update(dt)
             
         screen.fill("black")
+        
+        
         
         for i in drawable:
             i.draw(screen)
